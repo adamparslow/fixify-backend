e03aba2e38f455c9808b309ba228866f2cb9ba74
Adam Parslow <adam.parslow@hotmail.com>
2020-02-14 22:24:29 +1100

    Basic auth and user information

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.use(express.static('client/build'));
+
+const port = process.env.PORT || 8080;
+// const ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+// const port = 8080;
+
+app.get("*", (req, res) => {
+    let url = req.originalUrl.slice(1);
+    if (url === "") url = "index.html";
+    res.sendFile(path.join(__dirname, url));
+    // fs.readFile("index.html", (err, data) => {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/html');
+    //     res.end(data);
+    // });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
