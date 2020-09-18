651e356568e9ad578002f69f847ff067395b1977
adamparslow <adam.parslow@hotmail.com>
2020-09-18 19:06:42 +1000

    Merge in

---
+++ frontend/src/model/spotifyApi.js
@@ -0,0 +1,63 @@
+import tokenHandler from "./tokenHandler.js";
+
+const getMegamixRegistrationStatus = async () => {
+	const url = `/megamix/register?refresh_token=${tokenHandler.getRefreshToken()}`;
+
+	const response = await fetch(url);
+	return await response.json();
+};
+
+const registerForMegamix = async () => {
+	const url = constants.website + "/megamix/register";
+	const refreshToken = tokenHandler.getRefreshToken();
+	const body = {
+		refresh_token: refreshToken,
+	};
+
+	await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	});
+};
+
+const deregisterFromMegamix = async () => {
+	const url = "/megamix/register";
+	const refreshToken = tokenHandler.getRefreshToken();
+	const body = {
+		refresh_token: refreshToken,
+	};
+
+	await fetch(url, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	});
+};
+
+const createMegamix = async () => {
+	const url = "/megamix/generate";
+	const refreshToken = tokenHandler.getRefreshToken();
+	const body = {
+		refresh_token: refreshToken,
+	};
+
+	await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	});
+};
+
+export default {
+	getMegamixRegistrationStatus,
+	registerForMegamix,
+	deregisterFromMegamix,
+	createMegamix,
+};
