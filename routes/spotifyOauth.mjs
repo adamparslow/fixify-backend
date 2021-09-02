ca04ab3ad282cb76785103c4c940b96dfd139094
adamparslow <adam.parslow@hotmail.com>
2021-09-02 20:59:41 +1000

    Add frontend uri to .env

--- routes/spotifyOauth.mjs
+++ routes/spotifyOauth.mjs
@@ -90,7 +90,8 @@ router.get("/callback", (req, res) => {
 
 				// we can also pass the token to the browser to make requests from there
 				res.redirect(
-					"http://localhost:8080/#/auth/collect/" +
+					process.env.FRONTEND_URI + 
+					"#/auth/collect/" +
 						querystring.stringify({
 							access_token: access_token,
 							refresh_token: refresh_token,
