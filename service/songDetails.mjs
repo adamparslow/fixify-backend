85b5f2c5b0c28ee7147e49406710fcfeb95105d1
adamparslow <adam.parslow@hotmail.com>
2021-09-13 20:42:17 +1000

    Add check for song in playlists

--- service/songDetails.mjs
+++ service/songDetails.mjs
@@ -1,3 +1,4 @@
+
 export const getSongDetails = async (id, spotifyApi) => {
     const playlistNames = await getPlaylistsThatContainTheSong(id, spotifyApi)
 
@@ -7,7 +8,32 @@ export const getSongDetails = async (id, spotifyApi) => {
 }
 
 const getPlaylistsThatContainTheSong = async (id, spotifyApi) => {
-    const playlists = spotifyApi.getPlaylists()    
+    const containingPlaylists = [];
+    const promises = [];
 
-    return ['playlistName']
+    const allPlaylists = await spotifyApi.getPlaylists();
+    const userInfo = await spotifyApi.getMyUserID();
+    const userId = userInfo.id;
+    // console.log(userId);
+    console.log(id);
+
+    allPlaylists.forEach(playlist => {
+        promises.push((async () => {
+            if (playlist.owner.id !== userId) {
+                return;
+            }
+
+            const tracks = await spotifyApi.getPlaylistTracks(playlist.href);
+                // tracks.forEach(track => console.log(track.track.id));
+            if (tracks.some(track => track.track.id == id)) {
+                console.log(playlist.name);
+                containingPlaylists.push(playlist.name);
+            }
+        })());
+    });
+    await Promise.all(promises);
+
+    // console.log(playlists);
+
+    return containingPlaylists;
 }
