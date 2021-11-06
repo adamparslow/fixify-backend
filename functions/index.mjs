a72fa653c36f4e4426fc70520c1b673808b2f523
adamparslow <adam.parslow@hotmail.com>
2021-11-06 22:21:09 +1100

    Move to pubsub

--- functions/index.mjs
+++ functions/index.mjs
@@ -9,13 +9,16 @@ import megamixRoutes from "./routes/megamix.mjs";
 import spotifyRoutes from "./routes/spotifyApi.mjs";
 import concertWatchRoutes from "./routes/concertWatch.mjs";
 
-import megamixStorage from "./models/megamixStorage.mjs";
-import scheduler from "./models/scheduler.mjs";
+import megamixCreator from "./models/megamixCreator.mjs";
+
+const megamixSchedule = "0 19 * * *";
+
+export const megamix = functions.pubsub.schedule(megamixSchedule).onRun(async () => {
+	console.log("Running schedule");
+	await megamixCreator.generateMegamixes();
+});
 
 const expressApp = express();
-// const port = process.env.NODE_ENV == "production" ? process.env.PORT : 3000;
-
-scheduler.scheduleMegamixes();
 
 expressApp.use(express.static("client/build"))
 	.use(express.json({strict: false}))
@@ -26,10 +29,6 @@ expressApp.use(express.static("client/build"))
 	.use("/spotify", spotifyRoutes)
 	.use("/concert_watch", concertWatchRoutes)
 	.use("/", generalRoutes);
-
-// expressApp.listen(port, () => {
-// 	console.log(`Listening on port ${port}...`);
-// });
 
 export const app = functions.runWith({
 	timeoutSeconds: 300
